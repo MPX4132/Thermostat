ac415b31d260ae0e62ce4f2db872a22c05bad622
Matias Barcenas <MPX.4132@Gmail.com>
2017-01-09 00:54:49 -0600

    Removed outdated/redundant files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Thermostat
-A simplistic thermostat interface designed to serve as the foundation for a more complex system. This project is meant to run on limited-resource systems.
--- Temperature.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Temperature.cpp
-//  Thermostat
-//
-//  Created by Matias Barcenas on 11/12/16.
-//  Copyright © 2016 Matias Barcenas. All rights reserved.
-//
-
-#include "Temperature.hpp"
